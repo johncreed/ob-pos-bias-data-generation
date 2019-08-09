729c0338428d81d84b94cdf85cd7aebd5dd2b9fd
johncreed <hsiajuiyang5174@gmail.com>
2019-08-09 15:36:42 +0800

    Update add pos bias

--- grid-and-filter/add-pos-bias/add_bias_all.sh
+++ grid-and-filter/add-pos-bias/add_bias_all.sh
@@ -1,7 +1,18 @@
+# Config
 rd='random_filter'
 pr='propensious_filter'
 de='determined_filter'
+filter_data='trvate.90.ffm'
 
 python pos-bias.py $rd&
 python pos-bias.py $pr&
 python pos-bias.py $de&
+
+awk '{$1=""; print $0}' $filter_data > $filter_data.tmp &
+wait
+
+paste $rd.pos.bias $filter_data.tmp
+paste $pr.pos.bias $filter_data.tmp
+paste $de.pos.bias $filter_data.tmp
+
+
